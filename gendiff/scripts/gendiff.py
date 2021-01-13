18ad7abba60c2951d52633837d079eee66704b34
BasedOnEvidence <TestTypoTest@gmail.com>
2021-01-13 23:28:12 +0300

    Create init reference

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python
+import argparse
 
 
 def main():
-    pass
+    parser = argparse.ArgumentParser(description="Generate diff")
+    parser.add_argument("first_file")
+    parser.add_argument("second_file")
+    args = parser.parse_args()
+    print(args.accumulate(args.integers))
 
 
 if __name__ == "__main__":
