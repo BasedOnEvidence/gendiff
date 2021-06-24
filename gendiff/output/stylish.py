914dbd873bc26b3b62a51533751898aa43bef6b7
BasedOnEvidence <TestTypoTest@gmail.com>
2021-06-24 03:00:28 +0300

    Optimize stylish.py

--- gendiff/output/stylish.py
+++ gendiff/output/stylish.py
@@ -2,6 +2,8 @@ from gendiff.constants import (
     ADDED, CHANGED, REMOVED, NESTED
 )
 
+DICT = 'dict'
+
 DATA_OUTPUT_TEMPLATE = '{}{}: {}'
 END_OUTPUT_TEMPLATE = '{}{}'
 
@@ -17,72 +19,79 @@ def stringify(value):
         return value
 
 
-def stringify_complex_value(node, lines, depth):
-    for key in node:
-        indent = INDENT_MUL * depth
-        if isinstance(node[key], dict):
-            lines.append(DATA_OUTPUT_TEMPLATE.format(indent, key, '{'))
-            stringify_complex_value(node[key], lines, depth + 1)
-            lines.append(END_OUTPUT_TEMPLATE.format(indent, '}'))
-        else:
-            lines.append(
-                DATA_OUTPUT_TEMPLATE.format(indent, key, stringify(node[key]))
-            )
-
-
-def build_value(key, value, indent, sign, depth):
-    local_output = []
+def build_value(key, value, indent, sign):
     local_indent = indent[:-2] + sign + ' '
-    if isinstance(value, dict):
-        local_output.append(
-            DATA_OUTPUT_TEMPLATE.format(local_indent, key, '{')
+    return (
+        DATA_OUTPUT_TEMPLATE.format(
+            local_indent,
+            key,
+            stringify(value)
         )
-        stringify_complex_value(value, local_output, depth + 1)
-        local_indent = indent[:-2] + ' ' + ' '
-        local_output.append(END_OUTPUT_TEMPLATE.format(local_indent, '}'))
-    else:
-        local_output.append(
-            DATA_OUTPUT_TEMPLATE.format(
-                local_indent,
-                key,
-                stringify(value)
-            )
-        )
-    return local_output
+    )
 
 
-def inner(diff, output, depth=1):
+def inner(diff, output, status, depth=1):
     for elem in diff:
+        if status != DICT:
+            status = elem.status
         indent = depth * INDENT_MUL
-        if elem.status == NESTED:
+        if status == NESTED:
             output.append(DATA_OUTPUT_TEMPLATE.format(indent, elem.key, '{'))
-            inner(elem.value, output, depth + 1)
+            inner(elem.value, output, status, depth + 1)
             output.append(END_OUTPUT_TEMPLATE.format(indent, '}'))
-        elif elem.status == ADDED:
-            output.extend(
-                build_value(elem.key, elem.value, indent, '+', depth)
-            )
-        elif elem.status == REMOVED:
-            output.extend(
-                build_value(elem.key, elem.value, indent, '-', depth)
-            )
-        elif elem.status == CHANGED:
-            output.extend(
-                build_value(elem.key, elem.value[0], indent, '-', depth)
-            )
-            output.extend(
-                build_value(elem.key, elem.value[1], indent, '+', depth)
-            )
+        elif status == DICT:
+            if isinstance(diff[elem], dict):
+                output.append(DATA_OUTPUT_TEMPLATE.format(indent, elem, '{'))
+                inner(diff[elem], output, status, depth + 1)
+                output.append(END_OUTPUT_TEMPLATE.format(indent, '}'))
+            else:
+                output.append(
+                    build_value(elem, diff[elem], indent, ' ')
+                )
+        elif status == ADDED:
+            if isinstance(elem.value, dict):
+                output.append(build_value(elem.key, '{', indent, '+'))
+                inner(elem.value, output, DICT, depth + 1)
+                output.append(END_OUTPUT_TEMPLATE.format(indent, '}'))
+            else:
+                output.append(
+                    build_value(elem.key, elem.value, indent, '+')
+                )
+        elif status == REMOVED:
+            if isinstance(elem.value, dict):
+                output.append(build_value(elem.key, '{', indent, '-'))
+                inner(elem.value, output, DICT, depth + 1)
+                output.append(END_OUTPUT_TEMPLATE.format(indent, '}'))
+            else:
+                output.append(
+                    build_value(elem.key, elem.value, indent, '-')
+                )
+        elif status == CHANGED:
+            if isinstance(elem.value[0], dict):
+                output.append(build_value(elem.key, '{', indent, '-'))
+                inner(elem.value[0], output, DICT, depth + 1)
+                output.append(END_OUTPUT_TEMPLATE.format(indent, '}'))
+            else:
+                output.append(
+                    build_value(elem.key, elem.value[0], indent, '-')
+                )
+            if isinstance(elem.value[1], dict):
+                output.append(build_value(elem.key, '{', indent, '+'))
+                inner(elem.value[1], output, DICT, depth + 1)
+                output.append(END_OUTPUT_TEMPLATE.format(indent, '}'))
+            else:
+                output.append(
+                    build_value(elem.key, elem.value[1], indent, '+')
+                )
         else:
-            output.extend(
-                build_value(elem.key, elem.value, indent, ' ', depth)
-            )
+            output.append(build_value(elem.key, elem.value, indent, ' '))
     return output
 
 
 def render(diff):
     result = []
     result.append('{')
-    result.extend(inner(diff, []))
+    status = diff[0].status
+    result.extend(inner(diff, [], status))
     result.append('}')
     return '\n'.join(result)
